ecb21f29db8b238960a6a5a4528635f89c4e6401
Нечепоренко Степан <neceporenkostepan@gmail.com>
2023-07-12 14:27:23 +0300

    inherit `Memory` from `dict` instead of using `Memory.next_`

--- marasmatic/bases/Memory.py
+++ marasmatic/bases/Memory.py
@@ -1,7 +1,6 @@
 import random
 import typing
 import pydantic
-import dataclasses
 
 from ..Pair    import Pair
 from ..Base    import Base
@@ -10,20 +9,18 @@ from ..Pattern import Pattern
 
 
 @pydantic.dataclasses.dataclass(frozen = True, kw_only = True)
-class Memory(Base):
-
-	next_  : dict[Pattern, set[Pattern]] = dataclasses.field(default_factory = dict)
+class Memory(Base, dict[Pattern, set[Pattern]]):
 
 	def __ilshift__(self, p: Pair) -> typing.Self:
 
 		if p.previous:
-			if p.previous in self.next_:
-				self.next_[p.previous].add(p.current)
+			if p.previous in self:
+				self[p.previous].add(p.current)
 			else:
 				self <<= Pair(None, p.previous)
 
-		if p.current not in self.next_:
-			self.next_[p.current] = set()
+		if p.current not in self:
+			self[p.current] = set()
 
 		return self
 
@@ -31,8 +28,8 @@ class Memory(Base):
 
 		try:
 			if current is not None:
-				return random.choice((*self.next_[current],))
+				return random.choice((*self[current],))
 		except Exception:
 			pass
 
-		return random.choice((*self.next_.keys(),))
+		return random.choice((*self.keys(),))
